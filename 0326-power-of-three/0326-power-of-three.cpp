b18b23097c064ee14d648f0faf6a34a96eb2aa5d
SUBHAMxDXD <subhambarada2002@gmail.com>
2025-08-13 19:14:16 +0530

    Time: 0 ms (100%), Space: 9 MB (14.61%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // 1162261467 is 3^19, the largest power of 3 that fits in a signed 32-bit integer.
+        return (n > 0) && (1162261467 % n == 0);
+    }
+};
