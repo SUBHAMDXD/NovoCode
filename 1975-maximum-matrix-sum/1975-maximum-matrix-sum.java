a9b7e2f6a203d2ae302036f1f2fe1431ca30d4c4
SUBHAMxDXD <subhambarada2002@gmail.com>
2026-01-05 21:14:32 +0530

    Time: 6 ms (73.94%), Space: 68.1 MB (52.73%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        long sum = 0;
+        int negativeCount = 0;
+        int minAbs = Integer.MAX_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int val = matrix[i][j];
+
+                if (val < 0) {
+                    negativeCount++;
+                }
+
+                int absVal = Math.abs(val);
+                sum += absVal;
+                minAbs = Math.min(minAbs, absVal);
+            }
+        }
+
+        // If negatives are odd, one smallest absolute value must stay negative
+        if (negativeCount % 2 == 1) {
+            sum -= 2L * minAbs;
+        }
+
+        return sum;
+    }
+}
